3fbf7f24569a8710c31f18d20233b5fd09c2d469
Fanny VC <fanny.vancampe@gmail.com>
2024-11-23 18:19:16 +0100

    fetch: decrease

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,11 +3,17 @@ import React from 'react';
 function Home({
   onIncrement,
   counter,
+  onDecrement,
 }) {
+
   return (
     <div>
+      Vous avez cliqué {counter} fois
       <button onClick={onIncrement}>
-        Vous avez cliqué {counter} fois
+        + 
+      </button>
+      <button onClick={onDecrement}>
+        -
       </button>
     </div>
   );
