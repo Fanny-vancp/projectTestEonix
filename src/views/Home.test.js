0e8a85ab1ddc0a3722662f19161c9c8754854b02
Fanny VC <fanny.vancampe@gmail.com>
2024-11-24 15:03:02 +0100

    test: Home : calls onIncrement/onDecrement props when +/- button is clicked

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -1,21 +1,42 @@
 import Home from './Home';
 
 describe('Home Component', () =>{
+    const mockIncrementProps = jest.fn();
+    const mockDecrementProps = jest.fn();
+
     it('Render Home component correctly with counter props', () => {
         const homeComponent = Home({
-            onIncrement: jest.fn(), 
-            onDecrement: jest.fn(), 
+            onIncrement: mockIncrementProps, 
+            onDecrement: mockDecrementProps, 
             counter : 5
         });
 
         expect(homeComponent.props.children[0].props.children[1]).toBe(5);
     })
 
-    /*it('It should increase number correctly', () => {
+    it('calls onIncrement props when "+" button is clicked', () => {
+        const homeComponent = Home({
+            onIncrement: mockIncrementProps, 
+            onDecrement: mockDecrementProps, 
+            counter : 0
+        });
 
+        const increaseButton = homeComponent.props.children[1];
+        increaseButton.props.onClick();
+
+        expect(mockIncrementProps).toHaveBeenCalled();
     })
 
-    it('It should descrease number correctly', () => {
-        
-    })*/
+    it('calls onIncrement props when "-" button is clicked', () => {
+        const homeComponent = Home({
+            onIncrement: mockIncrementProps, 
+            onDecrement: mockDecrementProps, 
+            counter : 0
+        });
+
+        const descreaseButton = homeComponent.props.children[2];
+        descreaseButton.props.onClick();
+
+        expect(mockDecrementProps).toHaveBeenCalled();
+    })
 })
